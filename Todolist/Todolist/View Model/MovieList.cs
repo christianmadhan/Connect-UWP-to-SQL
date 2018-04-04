1b4864e04557075f0fc24bc38cb0192d8b7319fb
KongChristian <cmiw@hotmail.dk>
2018-04-04 15:38:35 +0200

    finnished

--- Todolist/Todolist/View Model/MovieList.cs
+++ Todolist/Todolist/View Model/MovieList.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Contacts;
 using Windows.UI.Popups;
+using Newtonsoft.Json;
 using Todolist.Model;
 
 namespace Todolist.View_Model
 {
     class MovieList : NotifyChanged
     {
+        private const string ServerUrl = "http://localhost:62394/"; 
+
         public Movie _selectedMovie;
 
         private readonly SingletonMovieList _singleton = SingletonMovieList.GetInstance();
@@ -23,6 +28,8 @@ namespace Todolist.View_Model
         public RelayCommand DeleteMovie { get; set; }
 
         public RelayCommand UpdateMovie { get; set; }
+
+        public Movie AddNewMovie { get; set; }
 
         //------------------------------------------------------------
         // Constructer
@@ -41,13 +48,97 @@ namespace Todolist.View_Model
             {
               Console.Write(e.Message);
             }
-        }
 
+            AddMovie = new RelayCommand(DoAddMovie);
+            DeleteMovie = new RelayCommand(DoDeleteMovie);
+            
+            AddNewMovie = new Movie();
+
+        }
 
         public Movie SelectedMovie
         {
             get => _selectedMovie;
             set { _selectedMovie = value; OnPropertyChanged(nameof(SelectedMovie)); }
+        }
+
+        public void DoAddMovie()
+        {
+            try
+            {
+                PostMovie("api/Movies/", AddNewMovie);
+                _singleton.GetMovieList().Add(AddNewMovie);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+
+        public  void DoDeleteMovie()
+        {
+            try
+            {
+                  DeleteMovieAsync("api/Movies/" + SelectedMovie.ID);
+                _singleton.GetMovieList().Remove(SelectedMovie);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+
+        // Http DeleteMovie Async Method
+
+        public static async Task<string> DeleteMovieAsync(string url)
+        {
+            HttpClientHandler handler = new HttpClientHandler() {UseDefaultCredentials = true};
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    HttpResponseMessage responseMessage = await client.DeleteAsync(url);
+                    if (responseMessage.IsSuccessStatusCode) await responseMessage.Content.ReadAsStringAsync();
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+
+        // Http Post/Add Movie Method.
+        public static async Task<string> PostMovie(string url, Movie objectToPost)
+        {
+            HttpClientHandler handler = new HttpClientHandler() { UseDefaultCredentials = true };
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var serializedString = JsonConvert.SerializeObject(objectToPost);
+                    StringContent content = new StringContent(serializedString, Encoding.UTF8, "application/json");
+                    HttpResponseMessage responseMessage = await client.PostAsync(url, content);
+                    if (responseMessage.IsSuccessStatusCode) return await responseMessage.Content.ReadAsStringAsync();
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
         }
     }
 }
